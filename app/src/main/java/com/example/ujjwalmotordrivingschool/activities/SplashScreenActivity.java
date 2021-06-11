8622b623113526d2c5971025fdacdc35b2077744
Aniket Patil <aniketp@siddhatech.com>
2021-06-11 23:52:27 +0530

    SplashScreenActivity changes

---
+++ app/src/main/java/com/example/ujjwalmotordrivingschool/activities/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.ujjwalmotordrivingschool.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ujjwalmotordrivingschool.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
